fb13dcde5e98a6323ee4c2d80227ddad87da7924
daniejl04 <danielestebanjimenezlopez@gmail.com>
2026-04-21 11:35:06 -0500

    Remove commented dotenv import and initialization from main.dart to clean up the code.

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,9 @@
 import 'package:flutter/material.dart';
-//import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:resetas/src/app.dart';
 import 'package:hive_ce_flutter/hive_ce_flutter.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  //await dotenv.load(); 
   await Hive.initFlutter();
   runApp(const MyApp());
 }
